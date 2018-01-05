56f2fb0955c33323573bd1b0008edef2556ffea8
YuvalZiv <zivhs.fam@gmail.com>
2018-01-05 21:57:08 +0200

    Initial

---
+++ DirectoryCompare/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstDirectoryPath = new System.Windows.Forms.TextBox();
+            this.SecondDirectoryPath = new System.Windows.Forms.TextBox();
+            this.FirstBrowse = new System.Windows.Forms.Button();
+            this.SecondBrowse = new System.Windows.Forms.Button();
+            this.FoundOutput = new System.Windows.Forms.TextBox();
+            this.Compare = new System.Windows.Forms.Button();
+            this.SummaryOutput = new System.Windows.Forms.TextBox();
+            this.NotFoundOutput = new System.Windows.Forms.TextBox();
+            this.Browser = new System.Windows.Forms.FolderBrowserDialog();
+            this.Conclusion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FirstDirectoryPath
+            // 
+            this.FirstDirectoryPath.Location = new System.Drawing.Point(12, 12);
+            this.FirstDirectoryPath.Name = "FirstDirectoryPath";
+            this.FirstDirectoryPath.Size = new System.Drawing.Size(367, 20);
+            this.FirstDirectoryPath.TabIndex = 0;
+            // 
+            // SecondDirectoryPath
+            // 
+            this.SecondDirectoryPath.Location = new System.Drawing.Point(12, 38);
+            this.SecondDirectoryPath.Name = "SecondDirectoryPath";
+            this.SecondDirectoryPath.Size = new System.Drawing.Size(367, 20);
+            this.SecondDirectoryPath.TabIndex = 1;
+            // 
+            // FirstBrowse
+            // 
+            this.FirstBrowse.Location = new System.Drawing.Point(385, 10);
+            this.FirstBrowse.Name = "FirstBrowse";
+            this.FirstBrowse.Size = new System.Drawing.Size(75, 23);
+            this.FirstBrowse.TabIndex = 2;
+            this.FirstBrowse.Text = "Browse...";
+            this.FirstBrowse.UseVisualStyleBackColor = true;
+            this.FirstBrowse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // SecondBrowse
+            // 
+            this.SecondBrowse.Location = new System.Drawing.Point(385, 36);
+            this.SecondBrowse.Name = "SecondBrowse";
+            this.SecondBrowse.Size = new System.Drawing.Size(75, 23);
+            this.SecondBrowse.TabIndex = 3;
+            this.SecondBrowse.Text = "Browse...";
+            this.SecondBrowse.UseVisualStyleBackColor = true;
+            this.SecondBrowse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // FoundOutput
+            // 
+            this.FoundOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.FoundOutput.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.FoundOutput.Location = new System.Drawing.Point(12, 64);
+            this.FoundOutput.Multiline = true;
+            this.FoundOutput.Name = "FoundOutput";
+            this.FoundOutput.ReadOnly = true;
+            this.FoundOutput.Size = new System.Drawing.Size(162, 173);
+            this.FoundOutput.TabIndex = 4;
+            this.FoundOutput.Text = "Found Output...";
+            // 
+            // Compare
+            // 
+            this.Compare.Location = new System.Drawing.Point(342, 64);
+            this.Compare.Name = "Compare";
+            this.Compare.Size = new System.Drawing.Size(115, 37);
+            this.Compare.TabIndex = 5;
+            this.Compare.Text = "Compare";
+            this.Compare.UseVisualStyleBackColor = true;
+            this.Compare.Click += new System.EventHandler(this.Compare_Click);
+            // 
+            // SummaryOutput
+            // 
+            this.SummaryOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.SummaryOutput.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.SummaryOutput.Location = new System.Drawing.Point(342, 107);
+            this.SummaryOutput.Multiline = true;
+            this.SummaryOutput.Name = "SummaryOutput";
+            this.SummaryOutput.ReadOnly = true;
+            this.SummaryOutput.Size = new System.Drawing.Size(115, 130);
+            this.SummaryOutput.TabIndex = 6;
+            this.SummaryOutput.Text = "Summary Output...";
+            // 
+            // NotFoundOutput
+            // 
+            this.NotFoundOutput.BackColor = System.Drawing.SystemColors.Window;
+            this.NotFoundOutput.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.NotFoundOutput.Location = new System.Drawing.Point(180, 64);
+            this.NotFoundOutput.Multiline = true;
+            this.NotFoundOutput.Name = "NotFoundOutput";
+            this.NotFoundOutput.ReadOnly = true;
+            this.NotFoundOutput.Size = new System.Drawing.Size(156, 173);
+            this.NotFoundOutput.TabIndex = 7;
+            this.NotFoundOutput.Text = "Not Found Output...";
+            // 
+            // Conclusion
+            // 
+            this.Conclusion.BackColor = System.Drawing.SystemColors.Window;
+            this.Conclusion.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Conclusion.Location = new System.Drawing.Point(12, 243);
+            this.Conclusion.Multiline = true;
+            this.Conclusion.Name = "Conclusion";
+            this.Conclusion.ReadOnly = true;
+            this.Conclusion.Size = new System.Drawing.Size(445, 40);
+            this.Conclusion.TabIndex = 8;
+            this.Conclusion.Text = "Conclusion...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 293);
+            this.Controls.Add(this.Conclusion);
+            this.Controls.Add(this.NotFoundOutput);
+            this.Controls.Add(this.SummaryOutput);
+            this.Controls.Add(this.Compare);
+            this.Controls.Add(this.FoundOutput);
+            this.Controls.Add(this.SecondBrowse);
+            this.Controls.Add(this.FirstBrowse);
+            this.Controls.Add(this.SecondDirectoryPath);
+            this.Controls.Add(this.FirstDirectoryPath);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstDirectoryPath;
+        private System.Windows.Forms.TextBox SecondDirectoryPath;
+        private System.Windows.Forms.Button FirstBrowse;
+        private System.Windows.Forms.Button SecondBrowse;
+        private System.Windows.Forms.TextBox FoundOutput;
+        private System.Windows.Forms.Button Compare;
+        private System.Windows.Forms.TextBox SummaryOutput;
+        private System.Windows.Forms.TextBox NotFoundOutput;
+        private System.Windows.Forms.FolderBrowserDialog Browser;
+        private System.Windows.Forms.TextBox Conclusion;
+    }
+}
+
---
+++ DirectoryCompare/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        private const int BYTES_TO_READ = sizeof(Int64);
+        private const string SummaryOutputFormat = "First Directory:\r\nFiles - {0}\r\nSub Directories - {1}\r\nSize - {2}B\r\n\r\nSecond Directory:\r\nFiles - {3}\r\nSub Directories - {4}\r\nSize - {5}B";
+        private const string FoundOutputFormat = "Found in both directories:\r\n{0}";
+        private const string NotFoundOutputFormat = "Not found in first directory:\r\n{0}\r\n\r\nNot found in second directory:\r\n{1}";
+        private const string ConclustionExistsAll = "All {0} files exists in both directories.";
+        private const string ConclustionNotAllExists = "{0} files from the first directory are missing in the second directory.\r\n{1} files from the second directory are missing in the first directory.";
+
+        Dictionary<Button, TextBox> BrowseButtonMatchingTextBox = new Dictionary<Button, TextBox>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BrowseButtonMatchingTextBox.Add(FirstBrowse, FirstDirectoryPath);
+            BrowseButtonMatchingTextBox.Add(SecondBrowse, SecondDirectoryPath);
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (Browser.ShowDialog() == DialogResult.OK)
+            {
+                BrowseButtonMatchingTextBox[(Button)sender].Text = Browser.SelectedPath;
+            }
+        }
+
+        private void Compare_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(FirstDirectoryPath.Text) || String.IsNullOrWhiteSpace(SecondDirectoryPath.Text))
+                MessageBox.Show("Empty folder path");
+
+            if (string.Equals(FirstDirectoryPath.Text, SecondDirectoryPath.Text, StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Please enter different directory paths.");
+                return;
+            }
+
+            ShowStats(FirstDirectoryPath.Text, SecondDirectoryPath.Text);                      
+            List<string> existsInBoth = new List<string>();
+            List<string> existsInFirstOnly = new List<string>();
+            List<string> existsInSecondOnly = new List<string>();
+            CompareTwoLists(FirstDirectoryPath.Text, SecondDirectoryPath.Text, TreeFolder(FirstDirectoryPath.Text), TreeFolder(SecondDirectoryPath.Text), ref existsInBoth, ref existsInFirstOnly, ref existsInSecondOnly);
+            ShowCompare(existsInBoth, existsInFirstOnly, existsInSecondOnly);
+        }
+
+        private void ShowStats(string firstDirectoryPath, string secondDirectoryPath)
+        {
+            long firstSizeInBytes = 0, secondSizeInBytes = 0;
+            int firstSubDirectoriesCount = 0, firstFilesCount = 0, secondSubDirectoriesCount = 0, secondFilesCount = 0;
+            GetStats(firstDirectoryPath, ref firstSizeInBytes, ref firstSubDirectoriesCount, ref firstFilesCount);
+            GetStats(secondDirectoryPath, ref secondSizeInBytes, ref secondSubDirectoriesCount, ref secondFilesCount);
+            SummaryOutput.Text = String.Format(SummaryOutputFormat, firstFilesCount, firstSubDirectoriesCount, firstSizeInBytes, secondFilesCount, secondSubDirectoriesCount, secondSizeInBytes);
+            SummaryOutput.ForeColor = Color.Black;
+        }
+
+        private void GetStats(string folderPath, ref long sizeInBytes, ref int subDirectoriesCount, ref int filesCount)
+        {
+            if (!(Directory.Exists(folderPath)))
+                MessageBox.Show("Unknown directory at " + folderPath);
+
+            foreach (string absoluteSubDirectory in Directory.GetDirectories(folderPath))
+            {
+                GetStats(absoluteSubDirectory, ref sizeInBytes, ref subDirectoriesCount, ref filesCount);
+                subDirectoriesCount++;
+            }
+
+            foreach (string absoluteFilePath in Directory.GetFiles(folderPath))
+            {
+                filesCount++;
+                sizeInBytes += new FileInfo(absoluteFilePath).Length;
+            }
+        }
+
+        private List<string> TreeFolder(string parentDirectoryPath)
+        {
+            return TreeFolder(parentDirectoryPath, parentDirectoryPath);
+        }
+
+        private List<string> TreeFolder(string parentDirectoryPath, string directoryPath)
+        {
+            List<string> files = new List<string>();
+
+            foreach (string absoluteSubDirectoryPath in Directory.GetDirectories(directoryPath))
+            {
+                files.AddRange(TreeFolder(parentDirectoryPath, absoluteSubDirectoryPath));
+            }
+
+            foreach (string absoluteFilePath in Directory.GetFiles(directoryPath))
+            {
+                files.Add(absoluteFilePath.Remove(0, parentDirectoryPath.Length));
+            }
+
+            return files;
+        }
+
+        private void CompareTwoLists(string firstDirectoryParent, string secondDirectoryParent, List<string> firstDirectoryTree, List<string> secondDirectoryTree, ref List<string> existsInBoth, ref List<string> existsInFirst, ref List<string> existsInSecond)
+        {
+            foreach (string file in firstDirectoryTree)
+            {
+                if (secondDirectoryTree.Contains(file) && CompareTwoFiles(firstDirectoryParent + file, secondDirectoryParent + file))
+                {
+                    secondDirectoryTree.Remove(file);
+                    existsInBoth.Add(file);
+                }
+                else
+                {
+                    existsInFirst.Add(file);
+                }
+            }
+
+            foreach (string file in secondDirectoryTree)
+            {
+                existsInSecond.Add(file);
+            }
+        }
+
+        private bool CompareTwoFiles(string firstFilePath, string secondFilePath)
+        {
+            if (firstFilePath == secondFilePath)
+                return true;
+
+            FileInfo firstFileInfo = new FileInfo(firstFilePath);
+            FileInfo secondFileInfo = new FileInfo(secondFilePath);
+
+            if (firstFileInfo.Length != secondFileInfo.Length)
+                return false;
+
+            return CompareData(firstFileInfo, secondFileInfo);
+            
+        }
+
+        private bool CompareData(FileInfo firstFileInfo, FileInfo secondFileInfo)
+        {
+            int iterations = (int)Math.Ceiling((double)firstFileInfo.Length / BYTES_TO_READ);
+
+            using (FileStream fs1 = firstFileInfo.OpenRead())
+            using (FileStream fs2 = secondFileInfo.OpenRead())
+            {
+                byte[] one = new byte[BYTES_TO_READ];
+                byte[] two = new byte[BYTES_TO_READ];
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    fs1.Read(one, 0, BYTES_TO_READ);
+                    fs2.Read(two, 0, BYTES_TO_READ);
+
+                    if (BitConverter.ToInt64(one, 0) != BitConverter.ToInt64(two, 0))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void ShowCompare(List<string> existsInBoth, List<string> existsInFirstOnly, List<string> existsInSecondOnly)
+        {
+            FoundOutput.Text = String.Format(FoundOutputFormat, String.Join("\r\n- ", existsInBoth));
+            NotFoundOutput.Text = String.Format(NotFoundOutputFormat, String.Join("\r\n- ", existsInFirstOnly), String.Join("\r\n- ", existsInSecondOnly));
+
+            if ((existsInFirstOnly.Count == 0) && (existsInSecondOnly.Count == 0))
+            {
+                Conclusion.Text = String.Format(ConclustionExistsAll, existsInBoth.Count);
+            }
+            else
+            {
+                Conclusion.Text = String.Format(ConclustionNotAllExists, existsInFirstOnly.Count, existsInSecondOnly.Count);
+            }
+
+            FoundOutput.ForeColor = Color.Black;
+            NotFoundOutput.ForeColor = Color.Black;
+            Conclusion.ForeColor = Color.Black;
+        }
+    }
+}
